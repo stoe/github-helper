c282e6be2abcc4f02182d51d0da3ea3607f7fccf
Stefan Stölzle <stoe@github.com>
2021-11-20 10:08:33 +0100

    v3.0.0 (#167)
    * 🆙 Update dependencies
    
    * 🤘 Refactor webpack build
    
    * 🤘 Remove build progress
    
    * 🚀 Bump version

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,9 +1,8 @@
 const path = require('path')
 
-const webpack = require('webpack')
 const cp = require('copy-webpack-plugin')
 const rm = require('remove-files-webpack-plugin')
-const sh = require('webpack-shell-plugin')
+const sh = require('webpack-shell-plugin-next')
 
 module.exports = {
   mode: 'production',
@@ -47,7 +46,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.ProgressPlugin(),
     new rm({
       before: {
         include: ['./build', './dist']
@@ -68,7 +66,14 @@ module.exports = {
       ]
     }),
     new sh({
-      onBuildEnd: ['script/build']
+      onBuildEnd: {
+        scripts: [
+          'mkdir build/',
+          './node_modules/.bin/crx3 -p key.pem -z ./build/github-helper.zip -o ./build/github-helper.crx ./dist/'
+        ],
+        blocking: false,
+        parallel: true
+      }
     })
   ]
 }
